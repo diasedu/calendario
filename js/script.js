5282b71caa2b687f169a690badbf470f3215d236
diasedu <eduardodias830@gmail.com>
2022-12-21 02:17:47 -0300

    Agora esta aplicação nos mostra o dia e o mês que estamos através das cores personalizadas(visíveis quando abrimos o calendário)

--- js/script.js
+++ js/script.js
@@ -1,18 +1,83 @@
-const month     = document.querySelectorAll('.month');
-const dayClnd   = document.querySelectorAll('.day');
-const date      = new Date;
+const month = document.getElementsByClassName('month');
+const dayClnd = document.getElementsByClassName('day');
+
+const date = new Date;
+
+const changeStyleMonth = (month) => month.setAttribute('id', 'currentMonth');
+const changeStyleDay   = (day)   => day.setAttribute('id', 'today');
 
 
+setInterval(function () {
 
+    for (let i = 0; i < month.length; i++) {
 
-dayClnd.forEach(function(e, i) {
-    const day = (dayClnd[i].textContent === (date.getDate().toString())) ? date.getDate() : '';
+        let monthInAttritube = month[i].getAttribute('id');
 
-    if (dayClnd[i].textContent === (date.getDate().toString())) {
-        dayClnd[i].style.backgroundColor = "#F0E68C";
-        dayClnd[i].style.color           = "gray";
-        alert(`Today is ${dayClnd[i].textContent}`)
+        switch (monthInAttritube) {
+
+            case 'january':
+                monthInAttritube = 0;
+                break;
+
+            case 'february':
+                monthInAttritube = 1;
+                break;
+
+            case 'march':
+                monthInAttritube = 2;
+                break;
+
+            case 'april':
+                monthInAttritube = 3;
+                break;
+
+            case 'may':
+                monthInAttritube = 6;
+                break;
+
+            case 'june':
+                monthInAttritube = 5;
+                break;
+
+            case 'july':
+                monthInAttritube = 6;
+                break;
+
+            case 'august':
+                monthInAttritube = 7;
+                break;
+
+            case 'september':
+                monthInAttritube = 8;
+                break;
+
+            case 'octuber':
+                monthInAttritube = 9;
+                break;
+
+            case 'nobember':
+                monthInAttritube = 10;
+                break;
+
+            case 'december':
+                monthInAttritube = 11;
+                break;
+        }
+
+        if (monthInAttritube === date.getMonth()) {
+
+            changeStyleMonth(month[i]);
+
+            for (let i = 0; i < dayClnd.length; i++) {
+
+                let dayInText = dayClnd[i].textContent;
+
+                if (dayInText === date.getDate().toString()) {
+
+                    changeStyleDay(dayClnd[i]);
+
+                }
+            }
+        }
     }
-    
-    
-});
+}, 1);
